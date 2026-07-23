f386e18e2369e76a12b19fc767efc443b28e9c3b
SumitOza <145322308+SumitOza@users.noreply.github.com>
2026-07-23 12:01:10 +0530

    Fetch nutrition info for selected fruits dynamically

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,7 @@ ingredients_list = st.multiselect(
     my_dataframe,
     max_selections=5
 )
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
 # Write directly to the app
 st.title(f"Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -27,7 +26,8 @@ if ingredients_list:
     
     for fruits_choosen in ingredients_list:
         ingredients_string += ' '+fruits_choosen+' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruit_choosen+'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_choosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     
     my_insert_stmt = """ insert into smoothies.public.orders(INGREDIENTS, NAME_ON_ORDER)
