88c11d9001b4a2cab3b91c461b8e6698fc4087f2
SumitOza <145322308+SumitOza@users.noreply.github.com>
2026-07-23 12:57:02 +0530

    Fix formatting of subheader in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ if ingredients_list:
     
     for fruits_choosen in ingredients_list:
         ingredients_string += ' '+fruits_choosen+' '
-        st.subheader(fruits_choosen + 'Nutrition Information')
+        st.subheader(fruits_choosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruits_choosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     
